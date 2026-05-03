6cf248eee8cb5e868cd5b06992b861224a1d9bf2
HadiHussain1 <hadi.ishfaque@gmail.com>
2026-05-03 16:35:09 +1000

    dev

--- app.py
+++ app.py
@@ -10031,6 +10031,11 @@ def stripe_start_checkout():
     return jsonify({"url": cs.url})
 
 
+@app.route('/uploads/<path:filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
+
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
